0ff3b256319990be44998cf7a57da98c5b8abb0b
Varun Hittuvalli <varun@MacBook-Pro-106.local>
2026-09-25 12:39:23 -0500

    feat(api): allow CORS requests from the Angular dev server

--- backend/main.py
+++ backend/main.py
@@ -4,8 +4,17 @@ from backend.routers import replays
 """don't store replay endpoints here use a 
 router so its easier to compile in main.py"""
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI(title="Telemetry Platform API")
 app.include_router(replays.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"], #allow angular access
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 #health endpoint can be stored here because it is unrelated to anything else
 @app.get("/health")
